6f40a6aee142c9cc37695e65c4554dc6f6523168
konglyyy <kongleyi99@gmail.com>
2025-10-03 21:32:24 +0800

    ci: Add ci pipeline

--- src/lib/team.ts
+++ src/lib/team.ts
@@ -1,13 +1,13 @@
 export const teamMembers = [
   {
     name: 'Kong Le-Yi',
-    role: 'Vibe Dreamer',
+    role: 'Not Your Average Vibe Engineer',
     avatar: '/images/kly.jpg',
     avatarSketch: '/images/kly-sketch.png',
   },
   {
     name: 'Tan Jin',
-    role: 'Vibe Dreamer',
+    role: 'Not Your Average Vibe Engineer',
     avatar: '/images/tj.jpg',
     avatarSketch: '/images/tj-sketch.png',
   },
